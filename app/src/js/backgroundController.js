4ab7ff17c3038c47e4fa5892861a671ede49ebf3
Sigtryggur Ómarsson <kvurit@gmail.com>
2015-04-02 23:43:12 +0000

    forgotten file.

--- app/src/js/backgroundController.js
+++ app/src/js/backgroundController.js
@@ -4,10 +4,12 @@ window.BackgroundController = (function() {
 
 	var fudgeFactor = 1;
 
+	var houseStartCount = 10;
 	var houseEndPosY = -5;
 	var houseSpawnChance = 0.03;
 	var houseSpawnCooldown = 0.7;
 
+	var cloudStartCount = 5;
 	var cloudEndPosY = -12;
 	var cloudSpawnChance = 0.01;
 	var cloudSpawnCooldown = 2.5;
@@ -23,6 +25,7 @@ window.BackgroundController = (function() {
 		this.lastCloudSpawnTime = 0;
 	};
 
+	//Runs on every frame, checks if it's time to spawn an new cloud or a house, or to remove one.
 	BackgroundController.prototype.onFrame = function(delta) {
 		var now = new Date() / 1000;
 		if(!this.gameState.gameEnded) {
@@ -39,6 +42,7 @@ window.BackgroundController = (function() {
 		this.cullClouds();
 	};
 
+	//Generates a random house dom object and appends it into the game
 	BackgroundController.prototype.spawnHouse = function() {
 		var type = Math.floor(Math.random() * 4);
 		var house = $('<div>').addClass('house');
@@ -54,9 +58,10 @@ window.BackgroundController = (function() {
 		}, 0.001);
 	};
 
+	//Generates a random cloud dom object and appends it into the game
 	BackgroundController.prototype.spawnCloud = function() {
 		var puffs = 5 + Math.floor(Math.random() * 5);
-		var offsetY = Math.random() * 15;
+		var offsetY = 10 + Math.random() * 15;
 		var cloud = $('<div>').addClass('cloud').css('top', offsetY + 'em');
 		var i, x, y, rand;
 
@@ -70,21 +75,29 @@ window.BackgroundController = (function() {
 		this.clouds.append(cloud);
 	};
 
+	//Populates the background with houses when player resets the game
 	BackgroundController.prototype.populateHouses = function() {
-		for(var i = 0; i < 10; i++) {
+		var step = 48/houseStartCount;
+		for(var i = 0; i < houseStartCount; i++) {
 			this.spawnHouse();
+			var house = this.houses.children()[i];
+			var offsetX = i * step + (Math.random() * step * 0.75);
+			$(house).css('left', offsetX + 'em');
 		}
-
-		this.houses.children().each(function(index, house) {
-			var offsetX = (Math.random() * 60) - 6;
-			$(house).css('left', offsetX+'em');
-		});
 	};
 
+	//Populates the background with clouds when player resets the game
 	BackgroundController.prototype.populateClouds = function() {
-		//todo
+		var step = 48/cloudStartCount;
+		for(var i = 0; i < cloudStartCount; i++) {
+			this.spawnCloud();
+			var cloud = this.clouds.children()[i];
+			var offsetX = (i * step + Math.random() * step * 0.5) - 60;
+			$(cloud).css('left', offsetX + 'em');
+		}
 	};
 
+	//Removes any offscreen house from the game
 	BackgroundController.prototype.cullHouses = function() {
 		var first = this.houses.children(':first');
 		var pos = first.position();
@@ -95,6 +108,7 @@ window.BackgroundController = (function() {
 		}
 	};
 
+	//Removes any offscreen cloud from the game
 	BackgroundController.prototype.cullClouds = function() {
 		var first = this.clouds.children(':first');
 		var pos = first.position();
@@ -105,19 +119,15 @@ window.BackgroundController = (function() {
 		}
 	};
 
+	//Stops the animation of the houses when the game ends.
 	BackgroundController.prototype.stop = function() {
-		this.houses.children().each(function(index, house) {
-			house = $(house);
+		var houses = this.houses.children();
+		for(var i = 0; i < houses.length; i++) {
+			var house = $(houses[i]);
 			var posX = house.position().left;
 			house.addClass('stop');
 			house.css('left', posX);
-		});
-		/*todo
-		this.clouds.children().each(function(index, cloud) {
-			cloud = $(cloud);
-			cloud.addClass('slow');
-		});
-		*/
+		}
 	};
 
 	BackgroundController.prototype.reset = function() {
